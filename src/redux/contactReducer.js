45a09303513059302e2fbe0f396d3a2b2a92282d
HalynaLastivka <lastivkagalina@gmail.com>
2023-10-25 17:22:33 +0300

    remove array

--- src/redux/contactReducer.js
+++ src/redux/contactReducer.js
@@ -1,12 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const INITIAL_STATE = {
-  contacts: [
-    { name: 'Rosie Simpson111', number: '459-12-56', id: 'id-1' },
-    { name: 'Hermione Kline', number: '443-89-12', id: 'id-2' },
-    { name: 'Eden Clements', number: '645-17-79', id: 'id-3' },
-    { name: 'Annie Copelan', number: '227-91-26', id: 'id-4' },
-  ],
+  contacts: [],
   filter: '',
 };
 
