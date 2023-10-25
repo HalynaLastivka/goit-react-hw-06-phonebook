6cc160f57265bede85842725cf8cc1c5fcfe0007
HalynaLastivka <lastivkagalina@gmail.com>
2023-10-25 17:17:02 +0300

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,84 @@
+import { nanoid } from 'nanoid';
+import ContactList from './ContactList/ContactList';
+
+import { Filter } from './Filter/Filter';
+import { FormPhone } from './FormPhone/FormPhone';
+// import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import {
+  addContact,
+  deleteContact,
+  setFilter,
+  setContacts,
+} from 'redux/contactReducer';
+
 export const App = () => {
+  const filter = useSelector(state => state.contacts.filter);
+
+  const contacts = useSelector(state => state.contacts.contacts);
+
+  const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   const stringifiedContacts = localStorage.getItem('contacts');
+  //   const parseContacts = JSON.parse(stringifiedContacts) ?? [];
+  //   // setContacts(parseContacts);
+  //   dispatch(setContacts(parseContacts));
+  // }, []);
+
+  // useEffect(() => {
+  //   const stringifiedContact = JSON.stringify(contacts);
+  //   localStorage.setItem('contacts', stringifiedContact);
+  // }, [contacts]);
+
+  const handleContact = (name, number) => {
+    console.log('helllo1111');
+    if (contacts.some(contact => contact.name === name)) {
+      alert(`${name} is already in contacts!`);
+      return;
+    }
+
+    const newOneContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    dispatch(addContact(newOneContact));
+  };
+
+  const handleDelete = contactId => {
+    dispatch(deleteContact(contactId));
+  };
+
+  const getFilteredContacts = () => {
+    if (!filter) {
+      return contacts;
+    }
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const onhandleChangeFilter = event => {
+    const { value } = event.target;
+    if (value !== undefined) {
+      dispatch(setFilter(value));
+    }
+  };
+
+  const filteredContacts = getFilteredContacts();
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <FormPhone handleContact={handleContact} />
+
+      <h2>Contacts</h2>
+      <Filter filter={filter} onChange={onhandleChangeFilter} />
+      <ContactList listContact={filteredContacts} handleDelete={handleDelete} />
     </div>
   );
 };
---
+++ src/redux/contactReducer.js
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const INITIAL_STATE = {
+  contacts: [
+    { name: 'Rosie Simpson111', number: '459-12-56', id: 'id-1' },
+    { name: 'Hermione Kline', number: '443-89-12', id: 'id-2' },
+    { name: 'Eden Clements', number: '645-17-79', id: 'id-3' },
+    { name: 'Annie Copelan', number: '227-91-26', id: 'id-4' },
+  ],
+  filter: '',
+};
+
+const contactSlice = createSlice({
+  // Ім'я слайсу
+  name: 'contactSlice',
+  // Початковий стан редюсера слайсу
+  initialState: INITIAL_STATE,
+  reducers: {
+    setFilter(state, action) {
+      state.filter = action.payload;
+    },
+    setContacts(state, action) {
+      state.contacts = action.payload;
+    },
+
+    addContact(state, action) {
+      state.contacts = [...state.contacts, action.payload];
+    },
+    deleteContact(state, action) {
+      state.contacts = state.contacts.filter(
+        contact => contact.id !== action.payload
+      );
+    },
+  },
+});
+
+// Генератори екшенів
+export const { setFilter, setContacts, addContact, deleteContact } =
+  contactSlice.actions;
+// Редюсер слайсу
+export const contactReducer = contactSlice.reducer;
